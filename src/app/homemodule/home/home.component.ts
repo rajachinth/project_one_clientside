b9e2467a0829bf1217e34592049f4400bc0aea3d
rajachinth <rajasekhar9040@outlook.com>
2020-11-05 12:39:11 +0530

    jquery fix

--- src/app/homemodule/home/home.component.ts
+++ src/app/homemodule/home/home.component.ts
@@ -1,7 +1,7 @@
 import { NgRedux, select } from '@angular-redux/store';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import * as $ from "jquery";
+import * as $ from 'jquery';
 import { RootStoreState } from 'src/app/storemodule/redux/corestore';
 
 @Component({
@@ -20,10 +20,10 @@ export class HomeComponent implements OnInit {
      $(document).ready(function() {
       // console.log('jquery element click');
       $('.header-brand-logo').trigger('click');
-    }); 
+    });
     // document.getElementsByClassName('header-brand-logo')
     //  docu  ment.getElementById('HeaderClick').click();
-  } 
+  }
 
-  
+
 }
